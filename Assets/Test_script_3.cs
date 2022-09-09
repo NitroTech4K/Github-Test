eb175725b7884d4d3c6762ddfd8b14c009a51c72
NitroTech 4K <crtraer@gmail.com>
2022-09-09 14:07:49 -0300

    Test_Script_3

--- Assets/Test_script_3.cs
+++ Assets/Test_script_3.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Test_script_3 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
